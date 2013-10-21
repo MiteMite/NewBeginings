17da209d8f767707ec1cb5aa197ba6af5d12a899
Simon <simon_fortier_drouin@live.ca>
2013-10-21 14:02:47 -0400

    added world to system print

--- src/newbeginings/NewBeginings.java
+++ src/newbeginings/NewBeginings.java
@@ -27,7 +27,7 @@ public class NewBeginings {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("hello world");
     }
     
 }
